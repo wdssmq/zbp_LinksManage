7c25fa9489da4be7fa6be7ac871b559e099bc03f
wdssmq <wdssmq@qq.com>
2020-04-04 10:52:52 +0800

    update

--- LinksManage/include.php
+++ LinksManage/include.php
@@ -73,6 +73,10 @@ function LinksManage_AddItem2Mod($item, $fileName)
   $mod = $zbp->GetModuleByFileName($fileName);
   $items = json_decode($mod->Metas->LM_json);
   $singlal = "";
+  // json在插件编辑页保存过才会生成
+  if (empty($items)) {
+    return;
+  }
   foreach ($items as $temp) {
     if (json_encode($temp) == json_encode($item)) {
       $singlal = "break";
