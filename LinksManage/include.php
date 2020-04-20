ed7f8f7945344466046755025e3941e83b84afd5
wdssmq <wdssmq@qq.com>
2020-04-20 19:28:50 +0800

    自动删除错误的usr模板

--- LinksManage/include.php
+++ LinksManage/include.php
@@ -30,10 +30,18 @@ function LinksManage_AddItemToNavbar($obj)
 function LinksManage_BuildTemp(&$templates)
 {
   // global $zbp;
-  if (is_file(LinksManage_Path("u-temp"))) {
-    $templates['lm-module-defend'] = file_get_contents(LinksManage_Path("u-temp"));
+  $uFile = LinksManage_Path("u-temp");
+  $vFile = LinksManage_Path("v-temp");
+  if (is_file($uFile)) {
+    $uFileCon = file_get_contents($uFile);
+    $templates['lm-module-defend'] = $uFileCon;
+    // 2020-04-20
+    if (strpos($uFileCon, '{$item.name}')) {
+      unlink(LinksManage_Path("u-temp"));
+    }
   } else {
-    $templates['lm-module-defend'] = file_get_contents(LinksManage_Path("v-temp"));
+    $vFileCon = file_get_contents($vFile);
+    $templates['lm-module-defend'] = $vFileCon;
   }
   $templates['lm-module-admin'] = file_get_contents(LinksManage_Path("tr"));
 }
